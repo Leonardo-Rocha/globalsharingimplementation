0af43735825841a946561817287d40bbb368f128
Leonardo C.A.Rocha <leonardocar.dm@gmail.com>
2018-12-03 00:25:05 -0300

    FINAL FIX script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Script para executar simuladores para vários traces e configurações, automaticamente.
 # Também organizar, de forma automatica, os resultados das execuções
 # em formato que possa ser transferido para planilha. - Os parâmetros de execução estão sempre na primeira linha e os resultados estão organizados em colunas.
@@ -11,10 +12,13 @@ do
   read tecnica_pred
   # echo "Digite o trace a ser utilizado:"
   read trace
-  if [[ "$tecnica_pred" == "nt" ]]; then
+  if [[ "$tecnica_pred" == "nt" ]];
+  then
     echo -e "sim_$i\t$tecnica_pred\t$trace" >> simulacoes.txt
     ./simpred $tecnica_pred $trace >> simulacoes.txt 
-  elif [[ "$tecnica_pred" == "cr" ]]; then
+  fi
+  if [[ "$tecnica_pred" == "cr" ]];
+  then
     # echo "Digite o numero de linhas"
     read num_linhas
     # echo "Digite m"
@@ -22,15 +26,23 @@ do
     # echo "Digite n"
     read n
     echo -e "sim_$i\t$tecnica_pred\t$trace\t$num_linhas\t$m\t$n" >> simulacoes.txt
-    ./simpred $tecnica_pred $trace $num_linhas $m $n>> simulacoes.txt 
-  else
+    ./simpred $tecnica_pred $trace $num_linhas $m $n >> simulacoes.txt 
+  fi
+  if [[ "$tecnica_pred" == "1b" ]];
+  then
     # echo "Digite o numero de linhas"
     read num_linhas
     echo -e "sim_$i\t$tecnica_pred\t$trace\t$num_linhas" >> simulacoes.txt
     ./simpred $tecnica_pred $trace $num_linhas >> simulacoes.txt 
   fi
-  echo >> simulacoes.txt
-  echo
+  if [[ "$tecnica_pred" == "2b" ]];
+  then
+    # echo "Digite o numero de linhas"
+    read num_linhas
+    echo -e "sim_$i\t$tecnica_pred\t$trace\t$num_linhas" >> simulacoes.txt
+    ./simpred $tecnica_pred $trace $num_linhas >> simulacoes.txt 
+  fi
+  #echo >> simulacoes.txt
   # echo "Deseja realizar mais simulações? [S/N]"
   read CONTINUE
   let "i++"
